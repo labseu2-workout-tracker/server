88bd9af9a0da725448eb4d73b3c601760a7faede
Becheru888 <becheru93@gmail.com>
2019-08-21 12:45:53 +0100

    Knex file configured

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const server = express();
 
-server.use(helmet());
-server.use(express.json());
-server.use(cors());
+const server = require('./api/server')
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'Workout Tracker API' });
 });
 
 const port = process.env.PORT || 5000;
+
 server.listen(port, console.log(`Listening on Port ${port}`));
