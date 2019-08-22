df8e31e5de423098ffb10269647acf9b948a5257
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-22 19:27:40 +0200

    getById | exercise-route

--- routes/exercise-route.js
+++ routes/exercise-route.js
@@ -12,4 +12,17 @@ router.get('/exercises', async (req, res) => {
   }
 });
 
+router.get("/exercises/:id", async (req, res) => {
+  const getExercise = await Exercise.getExerciseById(req.params.id);
+  try {
+    if(getExercise) {
+        res.status(200).json(getExercise);
+    } else {
+      res.status(400).json({ message: 'invalid Id !' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: `Cannot retrieve the exercise !` });
+  }
+});
+
   module.exports = router;
