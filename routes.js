1e5df41b73fc6e88327e36c9709ddb323bb47437
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-22 18:51:28 +0200

    get workout by id | routes

--- routes.js
+++ routes.js
@@ -12,4 +12,17 @@ router.get('/workouts', async (req, res) => {
   }
 });
 
+router.get("/workouts/:id", async (req, res) => {
+  const getWorkout = await Workout.getWorkoutById(req.params.id);
+  try {
+    if(getWorkout) {
+        res.status(200).json(getWorkout);
+    } else {
+      res.status(400).json({ message: 'invalid Id ' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: `Cannot retrieve the workout` });
+  }
+});
+
   module.exports = router;
