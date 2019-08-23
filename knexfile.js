d264510ca3a56c3bd9a54603ee782ac7dd362eae
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-08-23 09:49:29 +0100

    configured knex with postgres

---
+++ knexfile.js
@@ -0,0 +1,38 @@
+require('dotenv').config(); // load .env variables
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true, // used to avoid warning on console
+    migrations: {
+      directory: './models/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './models/seeds' },
+  },
+  testing: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './models/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: {
+      directory: './models/seeds',
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './models/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: {
+      directory: './models/seeds',
+    },
+  },
+};
