fad73ab90e6bcef161152808a02b72a7292fc50b
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-23 23:29:17 +0400

    add: setHeaders and auth route to index file

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const authRoutes = require('./routes/auth')
+
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Controll-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+server.use('/auth', authRoutes)
 
 server.get('/api', (req, res) => {
   res.status(200).json({ message: 'Workout Tracker API' });
