d20839de0fc52a932ded10c7f0269a7c0f925f93
Hoxtygen <hoxtygen@live.com>
2019-08-23 18:55:03 +0100

    Modify seeds directory in testing environemt

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
       tableName: 'dbmigrations',
     },
     seeds: {
-      directory: './models/seeds/test',
+      directory: './models/seeds',
     },
   },
   production: {
