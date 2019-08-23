4c09c5b617790bd4a083d04a773b64506f9dc5cf
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-24 00:17:07 +0400

    add: login controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,3 +32,40 @@ exports.signup = (req, res, next) => {
       next(err);
     });
 };
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  let loadedUser;
+  User.findByFilter({ email: email })
+    .then(user => {
+      if (!user) {
+        const error = new Error('A user with this email could not be found');
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then(isEqual => {
+      if (!isEqual) {
+        const error = new Error('Wrong password');
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser.id
+        },
+        'JWT SECRET',
+        { expiresIn: '24h' }
+      );
+      res.status(200).json({ token: token, userId: loadedUser.id });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
