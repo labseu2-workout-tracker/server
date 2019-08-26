8092cfdd44ed9366dedee299d070896e45a53633
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-26 11:06:57 +0200

    created get endpoint for getting all exercises in exercise-routes.js

--- routes/exercises/exercise-routes.js
+++ routes/exercises/exercise-routes.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const db = require('./exercise-helpers');
 
+router.get('/', (req, res) => {
+  db.findExercises()
+    .then(exercises => {
+      res.status(200).json(exercises);
+    })
+    .catch(err => {
+      res.status(500).json({ Error: 'Internal Server Error' });
+    });
+});
+
 router.get('/:id', (req, res) => {
   db.findExerciseById(req.params.id)
     .then(exercise => {
