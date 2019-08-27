4648e91f3a2d3ff27971e306753ec0635ee2fe7c
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-27 13:33:01 +0200

    deleted code that was not from me

--- exercises/exercisesModels.js
+++ exercises/exercisesModels.js
@@ -1,17 +1,9 @@
-const db = require('../../models/dbConfig');
+const db = require('../data/dbConfig');
 
 function findExercises() {
   return db('exercises');
 }
 
-function findExerciseById(id) {
-  return db('exercises')
-    .select()
-    .where({ id: id })
-    .first();
-}
-
 module.exports = {
   findExercises,
-  findExerciseById,
 };
--- exercises/exercisesRoutes.js
+++ exercises/exercisesRoutes.js
@@ -1,24 +1,10 @@
 const router = require('express').Router();
-const db = require('./exercise-helpers');
+const db = require('./exercisesModels');
 
 router.get('/', async (req, res) => {
   try {
     const exercises = await db.findExercises();
     res.status(200).json(exercises);
-  } catch (error) {
-    res.status(500).json({ Error: 'Internal Server Error' });
-  }
-});
-
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const exercise = await db.findExerciseById(id);
-    if (exercise) {
-      res.status(200).json(exercise);
-    } else {
-      res.status(400).json({ message: 'Invalid Id.' });
-    }
   } catch (error) {
     res.status(500).json({ Error: 'Internal Server Error' });
   }
