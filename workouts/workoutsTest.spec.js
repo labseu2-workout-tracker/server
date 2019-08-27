5df32b2c49e3c39f6a80f14e49b2a646feaed1d1
Hoxtygen <hoxtygen@live.com>
2019-08-27 20:40:10 +0100

    Add test to get all workouts

---
+++ workouts/workoutsTest.spec.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const server = require('../index');
+
+
+describe('Workouts', () => {
+  describe('[/get] workouts', () => {
+    it('should return all workouts', async () => {
+      const workouts = await request(server).get('/workouts');
+      expect(workouts.statusCode).toBe(200);
+    });
+  });
+});
