ea493766d490b8d74688de83718e25140ba9c46e
Becheru888 <becheru93@gmail.com>
2019-08-27 13:17:02 +0100

    workout-seed created

--- data/seeds/workouts.js
+++ data/seeds/workouts.js
@@ -5,9 +5,14 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {
+           id: 1,
+           workout_name: 'Squats',
+           workout_description: 'the best workout for logs',
+           image_url:'https://i.postimg.cc/bvLZzGrP/squats.jpg',
+           user_id:1
+          }
+           
       ]);
     });
 };
