d1a0ee6b7669d6877104b1070c5df8c414083546
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-27 17:35:12 +0400

    chore: refactor login endpoint from .then to try/catch

--- users/usersControllers.js
+++ users/usersControllers.js
@@ -28,41 +28,27 @@ exports.signup = (req, res, next) => {
     });
 };
 
-exports.login = (req, res, next) => {
-  const { email, password } = req.body;
-  let loadedUser;
-  User.findBy({ email })
-    .then(user => {
-      if (!user) {
-        const error = new Error(
-          'A user with this email could not be found',
-        );
-        error.statusCode = 401;
-        throw error;
-      }
-      loadedUser = user;
-      return bcrypt.compare(password, user.password);
-    })
-    .then(isEqual => {
-      if (!isEqual) {
-        const error = new Error('Wrong password');
-        error.statusCode = 401;
-        throw error;
-      }
-      const token = jwt.sign(
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findBy({ email });
+    if (user && bcrypt.compare(password, user.password)) {
+      const token = await jwt.sign(
         {
-          email: loadedUser.email,
-          userId: loadedUser.id,
+          email: user.email,
+          userId: user.id,
         },
         'JWT SECRET',
         { expiresIn: '24h' },
       );
-      res.status(200).json({ token, userId: loadedUser.id });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+      return res.status(200).json({ token, userId: user.id });
+    } else {
+      return res
+        .status(401)
+        .json({ message: 'Oops, username or password is incorrect' });
+    }
+  } catch (err) {
+    res.status(500).json(error);
+    next(err);
+  }
 };
