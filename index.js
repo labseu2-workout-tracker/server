60ec37ae9652ea9ec190a6a8dce45f6ad077e0c3
Hoxtygen <hoxtygen@live.com>
2019-08-27 22:59:54 +0100

    Fix EADDRINUSE :::5000 error

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const server = express();
 
 const workoutRoutes = require('./workouts/workoutsRoutes');
 const exerciseRouter = require('./exercises/exercisesRoutes');
+const workoutRouter = require('./workouts/workoutsRoutes');
 
 server.use(helmet());
 server.use(cors());
@@ -14,12 +15,23 @@ server.use(express.urlencoded({ extended: true }));
 
 server.use('/workouts', workoutRoutes);
 server.use('/exercises', exerciseRouter);
+server.use('/workouts', workoutRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'Workout Tracker API' });
 });
 
 const port = process.env.PORT || 5000;
-server.listen(port, console.log(`Listening on Port ${port}`));
+// server.listen(port, console.log(`Listening on Port ${port}`));
+
+/* 
+  With respect to listen EADDRINUSE :::5000 error returned when testing
+  The problem is this: once the execution of the first test ends, the server is still listening on the port 5000. So when we require('../in dex') again in the second test file, it errors out because is port is still in use by the previous test file’s process.
+  One simple fix for this issue is wrapping our app.listen() in a condition to check if the environment is a test environment or not. In a test environment, when running the server through Supertest, we do not really need to have the app listen on a network port.
+  For more read https://blog.campvanilla.com/jest-expressjs-and-the-eaddrinuse-error-bac39356c33a
+*/
+if (process.env.NODE_ENV !== 'test') {
+  server.listen(port, console.log(`Listening on Port ${port}`));
+}
 
 module.exports = server;
