d7254d063c6c88c7ca0b6fdaa7f274b4669dfadd
Hoxtygen <hoxtygen@live.com>
2019-08-27 16:00:12 +0100

    Add script to run sql file

--- data/seeds/02-workouts.js
+++ data/seeds/02-workouts.js
@@ -1,17 +1,16 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('workouts').del()
-    .then(function () {
+  return knex('workouts')
+    .del()
+    .then(function() {
       // Inserts seed entries
       return knex('workouts').insert([
         {
-           id: 1,
-           workout_name: 'Squats',
-           workout_description: 'the best workout for legs',
-           image_url:'https://i.postimg.cc/bvLZzGrP/squats.jpg',
-          }
-           
+          id: 1,
+          workout_name: 'Squats',
+          workout_description: 'the best workout for legs',
+          image_url: 'https://i.postimg.cc/bvLZzGrP/squats.jpg',
+        },
       ]);
     });
 };
