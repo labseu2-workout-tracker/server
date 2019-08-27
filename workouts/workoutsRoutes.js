13d8a7ee170a3d281d0326d87c17cd8d9b25441e
Becheru888 <becheru93@gmail.com>
2019-08-27 17:00:10 +0100

    latest

--- workouts/workoutsRoutes.js
+++ workouts/workoutsRoutes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const DB = require('./workoutsModels');
 
 router.get('/:id', async (req, res) => {
-  const exercises = await DB.getWorkoutExercises(req.params.id);
   try {
+    const exercises = await DB.getWorkoutExercises(req.params.id);
     res.status(200).json(exercises);
   } catch (error) {
     res.status(404).json('Not found');
