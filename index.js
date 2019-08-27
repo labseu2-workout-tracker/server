fcd29e6fe08a4085d938e430081ebd82581804d0
Matt Locklin <mattlocklin@live.co.uk>
2019-08-27 11:22:08 +0100

    Pulled latest branch for testing

--- index.js
+++ index.js
@@ -5,21 +5,17 @@ const cors = require('cors');
 const server = express();
 
 // Import exercise route
-const exerciseRouter = require('./routes/exercises/exercise-routes');
+const exerciseRouter = require('./exercises/exercisesRoutes');
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-<<<<<<< HEAD
 // Use imported exercise route
 server.use('/exercises', exerciseRouter);
 
 server.get('/', (req, res) => {
-=======
-server.get('/api', (req, res) => {
->>>>>>> 8de28c3b758460b0a0057b33eb5af5c040219ede
   res.status(200).json({ message: 'Workout Tracker API' });
 });
 
