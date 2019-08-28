d22088b3887df972a34bb1d474733e03afaf5067
Hoxtygen <hoxtygen@live.com>
2019-08-28 11:40:15 +0100

    Fix eslint/prettier error

--- data/seeds/02-workouts.js
+++ data/seeds/02-workouts.js
@@ -6,8 +6,14 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('workouts').insert([
         {
+<<<<<<< HEAD
           workout_name: 'Legs',
           workout_description: 'The perfect workout for your leg muscles',
+=======
+          id: 1,
+          workout_name: 'Squats',
+          workout_description: 'the best workout for legs',
+>>>>>>> Fix eslint/prettier error
           image_url: 'https://i.postimg.cc/bvLZzGrP/squats.jpg',
         },
       ]);
