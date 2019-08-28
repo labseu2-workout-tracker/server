94ee2d9e6dd7106e83d3381a7e302fcefbf99d43
Hoxtygen <hoxtygen@live.com>
2019-08-28 11:40:15 +0100

    Fix eslint/prettier error

--- data/seeds/03-workouts-exercise.js
+++ data/seeds/03-workouts-exercise.js
@@ -1,11 +1,11 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('workouts-exercises').del()
-    .then(function () {
+  return knex('workouts-exercises')
+    .del()
+    .then(function() {
       // Inserts seed entries
       return knex('workouts-exercises').insert([
-        {id: 1, exercise_id: 1, workout_id: 1},
+        { id: 1, exercise_id: 1, workout_id: 1 },
       ]);
     });
 };
