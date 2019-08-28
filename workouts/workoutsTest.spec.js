56b2c2018fc641f6411436f99e7082a2c1f0ed66
Hoxtygen <hoxtygen@live.com>
2019-08-28 20:12:23 +0100

    Update test file

--- workouts/workoutsTest.spec.js
+++ workouts/workoutsTest.spec.js
@@ -1,11 +1,30 @@
 const request = require('supertest');
 const server = require('../index');
 
+let token;
 describe('Workouts', () => {
+  beforeAll(done => {
+    request(server)
+      .post('/auth/login')
+      .send({
+        email: 'foobaz@gmail.com',
+        password: '123456',
+      })
+      .end((err, res) => {
+        token = res.body.token;
+        done();
+      });
+  });
+
   describe('[/get] workouts', () => {
-    it('should return all workouts', async () => {
-      const workouts = await request(server).get('/workouts');
-      expect(workouts.statusCode).toBe(200);
+    it('should return all workouts',  () => {
+      return request(server)
+        .get('/workouts')
+        .set('Authorization', `Bearer ${token}`)
+        .then(res => {
+          expect(res.statusCode).toBe(200);
+        });
+      // expect(workouts.statusCode).toBe(200);
     });
   });
 });
