ea73403efdf338cbc20039e28b9d8f03341ed9e4
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-30 16:30:34 +0200

    added try catch block

--- users/usersSettingsRoutes.js
+++ users/usersSettingsRoutes.js
@@ -3,6 +3,11 @@ const User = require('./usersModels');
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
+  try {
+   
+  } catch (error) {
+    res.status(500).json({ Error: 'Internal Server Error' });
+  }
 });
 
 module.exports = router;
