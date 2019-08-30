76463188ce3990ecd951769f5b80b8f8d5389afa
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-30 16:31:05 +0200

    added if statement in get endpoint

--- users/usersSettingsRoutes.js
+++ users/usersSettingsRoutes.js
@@ -4,7 +4,12 @@ const User = require('./usersModels');
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
   try {
-   
+    const settings = await User.findById(id);
+    if (settings) {
+      res.status(200).json(settings);
+    } else {
+      
+    }
   } catch (error) {
     res.status(500).json({ Error: 'Internal Server Error' });
   }
