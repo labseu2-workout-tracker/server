f66b7434626db76697ead12f86b4d2a26702682a
Matt Locklin <mattlocklin@live.co.uk>
2019-08-30 16:39:34 +0100

    Struggling with tests for put request. Tests fine using postman.

---
+++ users/usersSettingsTest.spec.js
@@ -0,0 +1,18 @@
+// const request = require('supertest');
+
+// const server = require('../index');
+
+// const userUpdate = {
+//   weight: 200,
+//   gender: 'female',
+//   height: 200,
+// };
+
+// describe('PUT/settings/:id', () => {
+//   it('Should response with 200 status code update user table and return json object containing userSettings  in full', async () => {
+//     const res = await request(server).put('/settings/1').send(userUpdate);
+//     expect(res.status).toBe(200);
+//     expect(res.type).toBe('application/json');
+//     expect(res.body).toBeInstanceOf(Object);
+//   });
+// });
