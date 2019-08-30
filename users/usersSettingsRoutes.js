ab47c882fca320687f321ae7a03c14e77287a0ad
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-30 16:30:20 +0200

    added id variable

--- users/usersSettingsRoutes.js
+++ users/usersSettingsRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const User = require('./usersModels');
 
 router.get('/:id', async (req, res) => {
-
+  const { id } = req.params;
 });
 
 module.exports = router;
