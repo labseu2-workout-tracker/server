3361f87c3332cb8b1ad662aa53626306fb70f03e
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-30 16:10:28 +0200

    delete settings route

--- users/usersRoutes.js
+++ users/usersRoutes.js
@@ -4,8 +4,6 @@ const { validateUser } = require('./usersHelper');
 
 const router = express.Router();
 
-router.get('/settings/:id', usersControllers.settings);
-
 router.post('/signup', validateUser, usersControllers.signup);
 
 router.post('/login', usersControllers.login);
