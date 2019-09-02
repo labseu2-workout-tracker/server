87d07c51c228d4393b3f8dadd17e3ab177f8177b
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-09-02 17:03:50 +0100

    reuse middleware for end workout endpoint

--- middlewares/checkWorkoutSession.js
+++ middlewares/checkWorkoutSession.js
@@ -1,13 +1,25 @@
 const workoutModel = require('../workouts/workoutsModels');
 
 module.exports = async (req, res, next) => {
+  // split endpoint and save the last path.
+  let endpoint = req.originalUrl.split('/');
+  endpoint = endpoint[endpoint.length - 1];
   const userSession = await workoutModel.findWorkoutSessionByUserId(
     req.userId,
   );
   if (userSession[0]) {
+    if (endpoint === 'end') {
+      [req.session] = userSession;
+      return next();
+    }
     return res.status(400).json({
       message:
         'You must finish your current workout before starting another',
+    });
+  }
+  if (endpoint === 'end') {
+    return res.status(400).json({
+      message: 'You do not have a workout in session',
     });
   }
   return next();
