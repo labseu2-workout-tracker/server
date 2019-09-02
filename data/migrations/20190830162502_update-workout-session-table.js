72e71e9d197982f1eddc1d89891446b7ea223203
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-09-02 09:46:49 +0100

    update workout-session table

---
+++ data/migrations/20190830162502_update-workout-session-table.js
@@ -0,0 +1,17 @@
+exports.up = function(knex) {
+  return knex.schema.table('workout-session', session => {
+    session
+      .integer('user_id')
+      .unsigned()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.table('workout-session', session => {
+    session.dropColumn('user_id');
+  });
+};
