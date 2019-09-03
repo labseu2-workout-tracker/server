31347e33d69352671ac50840b29675e64aaf4e3a
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-09-03 04:04:45 +0100

    write tests for workout history

--- workouts/workoutsTest.spec.js
+++ workouts/workoutsTest.spec.js
@@ -60,12 +60,12 @@ describe('Workouts', () => {
           expect(res.body.data).toBeDefined();
         });
     });
-    it('should return a status of 400 if workout id is invalid', () => {
+    it('should return a status of 404 if workout id is not a number', () => {
       return request(server)
         .get('/workouts/4ty')
         .set('Authorization', `Bearer ${token}`)
         .then(res => {
-          expect(res.statusCode).toBe(400);
+          expect(res.statusCode).toBe(404);
           expect(res.body.error).toBeDefined();
         });
     });
@@ -163,6 +163,41 @@ describe('Workouts', () => {
       const invalidToken = 'mhvhgjbhjkljkn098765754667854565';
       return request(server)
         .post('/workouts/2/end')
+        .set('Authorization', `Bearer ${invalidToken}`)
+        .then(res => {
+          expect(res.statusCode).toBe(401);
+        });
+    });
+  });
+
+  describe('GET /workouts/history - Gets all workout sessions by logged in user', () => {
+    it('should return a 200 and an array of user sessions', () => {
+      return request(server)
+        .get('/workouts/history')
+        .set('Authorization', `Bearer ${token}`)
+        .then(res => {
+          expect(res.statusCode).toBe(200);
+          expect(res.body.message).toBe(
+            'Workout History Retrieved Succesfully',
+          );
+          expect(res.body.workoutHistory).toBeInstanceOf(Array);
+        });
+    });
+    it('should return a 200 when a user tries to retrieve workout history with date limit', () => {
+      return request(server)
+        .get('/workouts/history?days=2')
+        .set('Authorization', `Bearer ${token}`)
+        .then(res => {
+          expect(res.statusCode).toBe(200);
+          expect(res.body.message).toEqual(
+            'Workout History Retrieved Succesfully',
+          );
+        });
+    });
+    it('should return a status of 401 if invalid token', () => {
+      const invalidToken = 'mhvhgjbhjkljkn098765754667854565';
+      return request(server)
+        .get('/workouts/history')
         .set('Authorization', `Bearer ${invalidToken}`)
         .then(res => {
           expect(res.statusCode).toBe(401);
