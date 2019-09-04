bcb135d0ce84effe094a502a11507172fe851156
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-09-04 07:20:48 +0100

    add updateProfile to profileRoutes

--- profile/profileRoutes.js
+++ profile/profileRoutes.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 const checkLoggedIn = require('../middlewares/restrictedMiddleware');
+const { validateUserInput } = require('./profileHelper');
 const profileControllers = require('./profileControllers');
 
 router.get('/', checkLoggedIn, profileControllers.getOne);
+
+router.put(
+  '/',
+  checkLoggedIn,
+  validateUserInput,
+  profileControllers.updateProfile,
+);
 
 module.exports = router;
