5bf15bad8af5ece6eeee6703d7a2b2ceadc4d4b7
Duduiala Ioana <becheru93@gmail.com>
2019-09-19 10:03:52 +0100

    table saved-workouts added

--- data/migrations/20190919091142_saved-workouts.js
+++ data/migrations/20190919091142_saved-workouts.js
@@ -1,23 +1,23 @@
 exports.up = function(knex) {
-    return knex.schema.createTable('saved-workouts', workout => {
-      workout.increments();
-      workout
-        .string('workout_name', 255)
-        .notNullable()
-        .unique();
-      workout.text('workout_description').notNullable();
-      workout.string('image_url', 255);
-      workout
-        .integer('user_id')
-        .unsigned()
-        .references('id')
-        .inTable('users')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE');
-      workout.timestamps(true, true);
-    });
-  };
-  
-  exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('saved-workouts');
-  };
+  return knex.schema.createTable('saved-workouts', workout => {
+    workout.increments();
+    workout
+      .string('workout_name', 255)
+      .notNullable()
+      .unique();
+    workout.text('workout_description').notNullable();
+    workout.string('image_url', 255);
+    workout
+      .integer('user_id')
+      .unsigned()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    workout.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('saved-workouts');
+};
