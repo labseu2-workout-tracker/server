ea61718b85d99778eee6d240217104abcf6cde03
Yusuf Ayo <haywhyze@gmail.com>
2019-09-25 09:51:31 +0100

    change tests assertions

--- exercises/exercisesModelsTest.spec.js
+++ exercises/exercisesModelsTest.spec.js
@@ -4,6 +4,6 @@ describe('get single exercise', () => {
   it('is able to get a single exercise by ID from the db!', async () => {
     const singleExercise = await exercise.findExerciseById('799');
     expect(singleExercise.id).toBe(799);
-    expect(singleExercise.exercise_name).toBe('Knee Across The Body');
+    expect(singleExercise.exercise_name).toBe('One Knee To Chest');
   });
 });
